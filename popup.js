b835ae5f61b1477f24b1be8f259d542e9afa8613
Cale Tilford <cale.tilford@ft.com>
2019-10-03 13:55:54 +0100

    Blur out Brexit content

--- popup.js
+++ popup.js
@@ -1,16 +1,30 @@
-let changeColor = document.getElementById('changeColor');
+let changeColor = document.getElementById("changeColor");
 
-chrome.storage.sync.get('color', function(data) {
-	changeColor.style.backgroundColor = data.color;
-	changeColor.setAttribute('value', data.color);
+chrome.storage.sync.get("color", function(data) {
+  changeColor.style.backgroundColor = data.color;
+  changeColor.setAttribute("value", data.color);
 });
 
+function filter() {
+  document.querySelectorAll(".o-teaser").forEach(function(el) {
+    const tag = el.querySelector(".o-teaser__tag");
+    if (tag) {
+      const label = tag
+        .getAttribute("aria-label")
+        .split("Category:")[1]
+        .trim();
+      if (label === "Brexit") {
+        el.style.filter = "blur(4px)";
+      }
+    }
+  });
+}
 
 changeColor.onclick = function(element) {
-	let color = element.target.value;
-	chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-		chrome.tabs.executeScript(tabs[0].id, {
-			code: 'document.body.style.backgroundColor = "' + color + '";',
-		});
-	});
+  let color = element.target.value;
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.executeScript(tabs[0].id, {
+      code: String(filter) + "filter();"
+    });
+  });
 };
