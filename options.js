da4b0ec0178f2094e93d24b5897d97cdd17611d2
Cale Tilford <cale.tilford@ft.com>
2019-10-04 11:30:24 +0100

    Add info about number of articles hiding

--- options.js
+++ options.js
@@ -1,22 +1,22 @@
-let page = document.getElementById('buttonDiv');
+let page = document.getElementById("buttonDiv");
 
-const kButtonColors = ['Brexit', 'royals', 'suicide', 'random'];
+const kButtonColors = ["Brexit", "Donald Trump"];
 
 function constructOptions(kButtonColors) {
-	for (let item of kButtonColors) {
-		let button = document.createElement('button');
-		button.innerHTML = item;
-		button.setAttribute('class', 'o-buttons o-buttons--big mg-16');
-		button.addEventListener('click', function() {
-			button.setAttribute(
-				'class',
-				'o-buttons o-buttons--primary o-buttons--big mg-16',
-			);
-			chrome.storage.sync.set({ text: item }, function() {
-				console.log('text is ' + item);
-			});
-		});
-		page.appendChild(button);
-	}
+  for (let item of kButtonColors) {
+    let button = document.createElement("button");
+    button.innerHTML = item;
+    button.setAttribute("class", "o-buttons o-buttons--big mg-16");
+    button.addEventListener("click", function() {
+      button.setAttribute(
+        "class",
+        "o-buttons o-buttons--primary o-buttons--big mg-16"
+      );
+      chrome.storage.sync.set({ text: item }, function() {
+        console.log("text is " + item);
+      });
+    });
+    page.appendChild(button);
+  }
 }
 constructOptions(kButtonColors);
--- popup.js
+++ popup.js
@@ -1,146 +1,151 @@
-let changeColor = document.getElementById('changeColor');
+let filterList = document.getElementById("filterList");
+let info = document.getElementById("info");
 
-chrome.storage.sync.get('text', function(data) {
-	changeColor.innerHTML = data.text;
-	changeColor.setAttribute('value', data.text);
+chrome.storage.sync.get("text", function(data) {
+  filterList.innerHTML = data.text;
+  filterList.setAttribute("value", data.text);
 });
 
-let toggleFilter = document.getElementById('filter-toggle');
-let toggleFilterLabel = document.getElementById('filter-toggle-label');
+let toggleFilter = document.getElementById("filter-toggle");
+let toggleFilterLabel = document.getElementById("filter-toggle-label");
 
 function filter(selectedOption) {
-	const teaserBlurredStyle = 'filter: blur(10px); pointer-events: none';
-	const overlayInitialStyle =
-		'position: absolute; top: 0; left: 0; height: 100%; width: 100%; display: flex; flex-direction: column; justify-content: center;';
+  const teaserBlurredStyle = "filter: blur(10px); pointer-events: none";
+  const overlayInitialStyle =
+    "position: absolute; top: 0; left: 0; height: 100%; width: 100%; display: flex; flex-direction: column; justify-content: center;";
 
-	chrome.storage.sync.get('isFiltered', function(data) {
-		if (!data.isFiltered) {
-			chrome.storage.sync.set({ isFiltered: true });
-			document.querySelectorAll('.o-teaser').forEach(function(el) {
-				let topNode = el;
-				const tag = topNode.querySelector('.o-teaser__tag');
+  chrome.storage.sync.get("isFiltered", function(data) {
+    if (!data.isFiltered) {
+      chrome.storage.sync.set({ isFiltered: true });
 
-				let isHidden = true;
+      let total = 0;
 
-				let label = '';
-				if (tag) {
-					label = tag
-						.getAttribute('aria-label')
-						.split('Category:')[1]
-						.trim();
-				}
+      document.querySelectorAll(".o-teaser").forEach(function(el) {
+        let topNode = el;
+        const tag = topNode.querySelector(".o-teaser__tag");
 
-				const titleNode = topNode.querySelector('.js-teaser-heading-link');
-				const standfirstNode = topNode.querySelector('.o-teaser__standfirst');
-				const title = titleNode ? titleNode.innerHTML : '';
+        let isHidden = true;
 
-				const standfirst = standfirstNode ? standfirstNode.innerHTML : '';
+        let label = "";
+        if (tag) {
+          label = tag
+            .getAttribute("aria-label")
+            .split("Category:")[1]
+            .trim();
+        }
 
-				if (
-					label === selectedOption ||
-					title.includes(selectedOption) ||
-					standfirst.includes(selectedOption)
-				) {
-					const isVideoPlayer = topNode
-						.getAttribute('class')
-						.includes('o-teaser--now-playing');
-					if (isVideoPlayer) {
-						topNode = el.parentNode;
-					}
+        const titleNode = topNode.querySelector(".js-teaser-heading-link");
+        const standfirstNode = topNode.querySelector(".o-teaser__standfirst");
+        const title = titleNode ? titleNode.innerHTML : "";
 
-					const wrapper = document.createElement('div');
-					const copied = topNode.cloneNode(true);
+        const standfirst = standfirstNode ? standfirstNode.innerHTML : "";
 
-					wrapper.setAttribute('class', 'teaser-wrapper');
-					wrapper.appendChild(copied);
-					wrapper.style = isVideoPlayer
-						? 'position: relative; width: 50%;'
-						: 'position: relative;';
-					copied.style = teaserBlurredStyle;
+        if (
+          label === selectedOption ||
+          title.includes(selectedOption) ||
+          standfirst.includes(selectedOption)
+        ) {
+          total += 1;
 
-					const overlay = document.createElement('div');
-					overlay.setAttribute('class', 'teaser-overlay');
-					const overlayText = document.createElement('div');
-					overlayText.setAttribute('class', 'teaser-overlay__text');
-					overlayText.innerHTML = 'You have hidden this article';
-					overlay.appendChild(overlayText);
-					wrapper.appendChild(overlay);
+          const isVideoPlayer = topNode
+            .getAttribute("class")
+            .includes("o-teaser--now-playing");
+          if (isVideoPlayer) {
+            topNode = el.parentNode;
+          }
 
-					const overlayButton = document.createElement('button');
-					overlayButton.innerHTML = 'Reveal';
-					overlayButton.style =
-						'position: absolute; top: 0; right: 0; border: none; padding: 5px 8px; z-index: 1;';
-					overlay.appendChild(overlayButton);
+          const wrapper = document.createElement("div");
+          const copied = topNode.cloneNode(true);
 
-					overlay.style = overlayInitialStyle;
-					overlayText.style = 'text-align: center;';
-					topNode.parentNode.replaceChild(wrapper, topNode);
+          wrapper.setAttribute("class", "teaser-wrapper");
+          wrapper.appendChild(copied);
+          wrapper.style = isVideoPlayer
+            ? "position: relative; width: 50%;"
+            : "position: relative;";
+          copied.style = teaserBlurredStyle;
 
-					overlayButton.onclick = function(buttonEl) {
-						if (isHidden) {
-							overlayText.style = 'text-align: center; display: none';
-							copied.style = '';
-							overlay.style.left = 'auto';
-							overlay.style.right = 0;
-							overlay.style.width = 'auto';
-							isHidden = false;
-							overlayButton.innerHTML = 'Hide';
-						} else {
-							overlayText.style = 'text-align: center;';
-							copied.style = teaserBlurredStyle;
-							overlay.style = overlayInitialStyle;
-							isHidden = true;
-							overlayButton.innerHTML = 'Reveal';
-						}
-					};
-				}
-			});
-		} else {
-			chrome.storage.sync.set({ isFiltered: false });
+          const overlay = document.createElement("div");
+          overlay.setAttribute("class", "teaser-overlay");
+          const overlayText = document.createElement("div");
+          overlayText.setAttribute("class", "teaser-overlay__text");
+          overlayText.innerHTML = "You have hidden this article";
+          overlay.appendChild(overlayText);
+          wrapper.appendChild(overlay);
 
-			document.querySelectorAll('.teaser-wrapper').forEach(function(el) {
-				const originalEl = el.querySelector('.o-teaser');
-				const isVideoPlayer = originalEl
-					.getAttribute('class')
-					.includes('o-teaser--now-playing');
+          const overlayButton = document.createElement("button");
+          overlayButton.innerHTML = "Reveal";
+          overlayButton.style =
+            "position: absolute; top: 0; right: 0; border: none; padding: 5px 8px; z-index: 1;";
+          overlay.appendChild(overlayButton);
 
-				if (isVideoPlayer) {
-					const videoEl = el.querySelector('.video-section__column--player');
-					el.parentNode.replaceChild(videoEl, el);
-					videoEl.style = 'none';
-				} else {
-					el.parentNode.replaceChild(originalEl, el);
-					originalEl.style = 'none';
-				}
-			});
-		}
-	});
+          overlay.style = overlayInitialStyle;
+          overlayText.style = "text-align: center;";
+          topNode.parentNode.replaceChild(wrapper, topNode);
+
+          overlayButton.onclick = function(buttonEl) {
+            if (isHidden) {
+              overlayText.style = "text-align: center; display: none";
+              copied.style = "";
+              overlay.style.left = "auto";
+              overlay.style.right = 0;
+              overlay.style.width = "auto";
+              isHidden = false;
+              overlayButton.innerHTML = "Hide";
+            } else {
+              overlayText.style = "text-align: center;";
+              copied.style = teaserBlurredStyle;
+              overlay.style = overlayInitialStyle;
+              isHidden = true;
+              overlayButton.innerHTML = "Reveal";
+            }
+          };
+        }
+      });
+      chrome.storage.sync.set({ total: total });
+    } else {
+      chrome.storage.sync.set({ isFiltered: false });
+
+      document.querySelectorAll(".teaser-wrapper").forEach(function(el) {
+        const originalEl = el.querySelector(".o-teaser");
+        const isVideoPlayer = originalEl
+          .getAttribute("class")
+          .includes("o-teaser--now-playing");
+
+        if (isVideoPlayer) {
+          const videoEl = el.querySelector(".video-section__column--player");
+          el.parentNode.replaceChild(videoEl, el);
+          videoEl.style = "none";
+        } else {
+          el.parentNode.replaceChild(originalEl, el);
+          originalEl.style = "none";
+        }
+      });
+    }
+  });
 }
 
 toggleFilter.onclick = function(element) {
-	chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-		chrome.tabs.executeScript(tabs[0].id, {
-			code: String(filter) + 'filter();',
-		});
-	});
+  chrome.storage.sync.get("isFiltered", function(data) {
+    toggleFilterLabel.innerHTML = !data.isFiltered ? "ON" : "OFF";
 
-	chrome.storage.sync.get('isFiltered', function(data) {
-		toggleFilterLabel.innerHTML = !data.isFiltered ? 'ON' : 'OFF';
-	});
-};
+    chrome.storage.sync.get("total", function(d) {
+      if (!data.isFiltered) {
+        info.innerHTML = `Currently hiding ${d.total} articles`;
+        filterList.setAttribute("value", d.total);
+      } else {
+        info.innerHTML = "";
+        filterList.setAttribute("value", "");
+      }
+    });
 
-changeColor.onclick = function(element) {
-	let color = element.target.value;
+    chrome.storage.sync.get("text", function(data) {
+      let selectedOption = data.text;
 
-	chrome.storage.sync.get('text', function(data) {
-		let selectedOption = data.text;
-		console.log(selectedOption);
-		if (selectedOption === 'Brexit') {
-			chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-				chrome.tabs.executeScript(tabs[0].id, {
-					code: String(filter) + `filter('${selectedOption}')`,
-				});
-			});
-		}
-	});
+      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        chrome.tabs.executeScript(tabs[0].id, {
+          code: String(filter) + `filter('${selectedOption}')`
+        });
+      });
+    });
+  });
 };
