2207a27bdaa46bfd41d5394e0de8a53559afeba0
Cale Tilford <cale.tilford@ft.com>
2019-10-04 12:41:28 +0100

    Add option for extra filters

--- options.js
+++ options.js
@@ -1,6 +1,27 @@
 let page = document.getElementById("buttonDiv");
+let form = document.getElementById("addNewForm");
 
-const kButtonColors = ["Brexit", "Donald Trump", "House"];
+let kButtonColors = ["Brexit", "Donald Trump", "House", "Tech", "Fall"];
+
+function addEventListenerToButton(button, item) {
+  button.addEventListener("click", function() {
+    button.setAttribute(
+      "class",
+      "o-buttons o-buttons--primary o-buttons--big mg-16"
+    );
+    chrome.storage.sync.get("text", function(data) {
+      let arr = data.text;
+      if (!arr.includes(item)) {
+        arr.push(item);
+      } else {
+        arr = arr.filter(d => d !== item);
+        button.setAttribute("class", "o-buttons o-buttons--big mg-16");
+      }
+      chrome.storage.sync.set({ text: arr });
+    });
+  });
+  page.appendChild(button);
+}
 
 function constructOptions(kButtonColors) {
   for (let item of kButtonColors) {
@@ -18,23 +39,22 @@ function constructOptions(kButtonColors) {
       }
     });
 
-    button.addEventListener("click", function() {
-      button.setAttribute(
-        "class",
-        "o-buttons o-buttons--primary o-buttons--big mg-16"
-      );
-      chrome.storage.sync.get("text", function(data) {
-        let arr = data.text;
-        if (!arr.includes(item)) {
-          arr.push(item);
-        } else {
-          arr = arr.filter(d => d !== item);
-          button.setAttribute("class", "o-buttons o-buttons--big mg-16");
-        }
-        chrome.storage.sync.set({ text: arr });
-      });
-    });
-    page.appendChild(button);
+    addEventListenerToButton(button, item);
   }
 }
 constructOptions(kButtonColors);
+
+addNewForm.onsubmit = function(e) {
+  e.preventDefault();
+  let v = document.getElementById("addNewInput").value;
+
+  if (!kButtonColors.includes(v)) {
+    kButtonColors.push(v);
+    let button = document.createElement("button");
+    button.innerHTML = v;
+    button.setAttribute("class", "o-buttons o-buttons--big mg-16");
+    addEventListenerToButton(button, v);
+
+    document.getElementById("addNewInput").value = "";
+  }
+};
